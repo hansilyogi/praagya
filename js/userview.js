91c2f965383fb82034fc16bcec1158c952a917dc
hansilyogi <hansilyogi@gmail.com>
2020-11-20 17:06:37 +0530

    20-November-4

--- js/userview.js
+++ js/userview.js
@@ -34,6 +34,7 @@ $(document).ready(function () {
                 "  data-id=" +
                 data.data._id +
                 ' data-up = 1 aria-hidden="true"></i>';
+          if(!data.data.schoolAddress){
           $("#displaydata").append(
             "<tr><td>Name</td><td>" +
               data.data.name +
@@ -95,6 +96,70 @@ $(document).ready(function () {
               status +
               "</td></tr>"
           );
+          }
+          else{
+            $("#displaydata").append(
+              "<tr><td>Name</td><td>" +
+                data.data.name +
+                "</td></tr>" +
+                "<tr><td>Membership Number</td><td>" +
+                data.data.stateCode +
+                data.data.affiliationCode +
+                "-" +
+                data.data.membershipNumber +
+                "</td></tr>" +
+                "<tr><td>Certificate Link</td><td>" +
+                "<a target=_blank href='http://15.206.249.190/api/" +
+                data.data.membershipPDF +
+                "'>Click Here</a>" +
+                "</td></tr>" +
+                "<tr><td>Email</td><td>" +
+                data.data.email +
+                "</td></tr>" +
+                "<tr><td>Personal Number</td><td>" +
+                data.data.personalNumber +
+                "</td></tr>" +
+                "<tr><td>Whatsapp Number</td><td>" +
+                data.data.whatsappNumber +
+                "</td></tr>" +
+                "<tr><td>Country</td><td>" +
+                data.data.country.Name +
+                "</td></tr>" +
+                "<tr><td>State</td><td>" +
+                data.data.state.name +
+                "</td></tr>" +
+                "<tr><td>City</td><td>" +
+                data.data.city +
+                "</td></tr>" +
+                "<tr><td>Gender</td><td>" +
+                gen +
+                "</td></tr>" +
+                "<tr><td>DOB</td><td>" +
+                data.data.dob +
+                "</td></tr>" +
+                "<tr><td>School Name</td><td>" +
+                data.data.schoolName +
+                "</td></tr>" +
+                "<tr><td>School Address</td><td>" +
+                data.data.schoolAddress +
+                "</td></tr>" +
+                "<tr><td>School Location</td><td>" +
+                "<a target='_blank' href='https://www.google.com/maps?q=" +
+                data.data.schoolLocation +
+                "'>" +
+                "Click Here</a>" +
+                "</td></tr>" +
+                "<tr><td>Affiliated With</td><td>" +
+                data.data.affilatedWith.Name +
+                "</td></tr>" +
+                "<tr><td>Affiliated Number</td><td>" +
+                data.data.afillatedNumber +
+                "</td></tr>" +
+                "<tr><td>Status</td><td>" +
+                status +
+                "</td></tr>"
+            );
+          }
         } else {
           $("#displaydata").html(
             '<tr><td colspan="5" class="text-center font-weight-bold">No Record Found</td></tr></center>'
